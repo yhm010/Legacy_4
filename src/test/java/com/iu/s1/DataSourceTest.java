72270b7af5600346284b26ba6b65877c7cb8a870
사용자 이름 <yoohm>
2022-02-17 12:07:32 +0900

    20220217

---
+++ src/test/java/com/iu/s1/DataSourceTest.java
@@ -0,0 +1,28 @@
+package com.iu.s1;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DataSourceTest extends MyJunitTest {
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	private SqlSession session;
+	
+	@Test
+	public void connectionTest() throws Exception {
+		assertNotNull(dataSource.getConnection());
+	}
+	@Test
+	public void sessionTest() {
+		assertNotNull(session);
+	}
+
+}
