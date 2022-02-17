93f7b07c473a162e8d7b7deeacbc22047c89aa73
사용자 이름 <yoohm>
2022-02-18 00:36:30 +0900

    20220217 Notice

---
+++ src/main/java/com/iu/s1/notice/NoticeDTO.java
@@ -0,0 +1,41 @@
+package com.iu.s1.notice;
+
+public class NoticeDTO {
+
+	private Integer num;	
+	private String title;
+	private String contents;
+	private String wrier;
+	private String regdate;
+	
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getWrier() {
+		return wrier;
+	}
+	public void setWrier(String wrier) {
+		this.wrier = wrier;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+}
---
+++ src/test/java/com/iu/s1/NoticeTest/NoticeDAOTest.java
@@ -0,0 +1,61 @@
+package com.iu.s1.NoticeTest;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.iu.s1.MyJunitTest;
+import com.iu.s1.notice.NoticeDAO;
+import com.iu.s1.notice.NoticeDTO;
+
+public class NoticeDAOTest extends MyJunitTest {
+
+	@Autowired
+	private NoticeDAO noticeDAO;
+
+	@Test
+	public void check() {
+		assertNotNull(noticeDAO);
+	}
+
+	// list
+	@Test
+	public void listTest() throws Exception {
+		List<NoticeDTO> ar = noticeDAO.list();
+		assertNotEquals(0, ar.size());
+	}
+
+	// add
+	//@Test
+	public void addTest() throws Exception {
+		NoticeDTO noticeDTO = new NoticeDTO();
+		noticeDTO.setTitle("t2");
+		noticeDTO.setContents("c2");
+		noticeDTO.setWrier("w1");
+		noticeDTO.setRegdate("220218");
+		int result = noticeDAO.add(noticeDTO);
+		assertEquals(1, result);
+	}
+
+	// detail
+	@Test
+	public void detailTest() throws Exception {
+		NoticeDTO noticeDTO = new NoticeDTO();
+		noticeDTO.setNum(6);
+		noticeDTO = noticeDAO.detail(noticeDTO);
+		assertNotNull(noticeDTO);
+	}
+	
+	//delete
+	@Test
+	public void deleteTest() throws Exception {
+		NoticeDTO noticeDTO = new NoticeDTO();
+		noticeDTO.setNum(8);
+		int result = noticeDAO.delete(noticeDTO);
+		assertEquals(8, result);
+	}
+
+}
